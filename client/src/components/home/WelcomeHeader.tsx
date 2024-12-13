5106456c3f22272e71af91d82d287af4ea39de7b
Sahil Mehra <sahilpmehra@yahoo.co.in>
2024-12-12 17:43:09 -0800

    Changing favicon and adding logo for the app

--- client/src/components/home/WelcomeHeader.tsx
+++ client/src/components/home/WelcomeHeader.tsx
@@ -5,6 +5,7 @@ interface WelcomeHeaderProps {
 
 const WelcomeHeader = ({ title, description }: WelcomeHeaderProps) => (
     <div className="text-center mb-8">
+        <img src="/logo.svg" alt="Logo" className="w-16 h-16 mx-auto mb-4" />
         <h1 className="text-3xl font-bold text-gray-800">{title}</h1>
         <p className="text-gray-600 mt-2">{description}</p>
     </div>
